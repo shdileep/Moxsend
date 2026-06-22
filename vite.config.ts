bf8dbce3044cf19395f11476838291bc0e4d6018
shdileep <140104018+shdileep@users.noreply.github.com>
2026-06-22 21:44:39 +0530

    fix: resolve WarningIssue in vite.config.ts:23

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,7 @@ export default defineConfig(({ mode }) => {
       hmr: process.env.DISABLE_HMR !== 'true',
       proxy: {
         '/api': {
-          target: 'http://127.0.0.1:3001',
+          target: process.env.API_URL || "http://127.0.0.1:3001",
           changeOrigin: true
         }
       }
